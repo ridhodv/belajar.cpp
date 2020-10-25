f904f773a680f15a307e4fa60a9b3136e4e902b9
RidhoCode <46701110+RidhoCode01@users.noreply.github.com>
2020-10-25 17:04:28 +0800

    Add files via upload

---
+++ tipe-data.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() {
+//membuat variabel
+    int test = 001;
+    float testFloat = 7.99;
+    double testDouble = 5.22;
+    bool testBoolean = true; 
+    char testKarakter = 'R';
+    string testString = "Hallo Ridho";
+
+//cetak variabel
+cout << "int: " << test << "\n";
+cout << "float:" << testFloat << "\n";
+cout << "double:" << testDouble << "\n";
+cout << "bool: " << testBoolean << "\n";
+cout << "char: " << testKarakter << "\n";
+cout << "string: " << testString << "\n";
+
+return 0;
+}
