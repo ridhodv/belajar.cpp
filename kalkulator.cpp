7d9fc9b96cd21a1fe544df59ae0c1d7a9dd59c2a
RidhoCode <46701110+RidhoCode01@users.noreply.github.com>
2020-10-25 17:08:12 +0800

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {  //contoh kalkulator
+    int x, y; 
+    int sum; 
+    cout << "Masukan angka pertama: ";
+    cin >> x; 
+    cout << "Masukan angka kedua: ";
+    cin >> y; 
+    sum = x + y; 
+    cout << "Hasil dari angka: " << sum; 
+    return 0;
+}
